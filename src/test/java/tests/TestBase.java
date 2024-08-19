0f72fbe12e2822ef4cd265a83af8fb1e8e6c5ee6
GeorgiiBubnov <georgii.bubnov.spb@gmail.com>
2024-08-19 19:16:18 +0300

    Add Owner

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,8 +3,10 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.WebDriverConfig;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -14,14 +16,19 @@ import java.util.Map;
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
+        WebDriverConfig config = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
 
         Configuration.baseUrl = System.getProperty("baseUrl", "https://patterns.games");
         Configuration.browserSize = System.getProperty("browserSize", "1980x1080");
-        Configuration.remote = "https://user1:1234@" + System.getProperty("remoteUrl",
-                "selenoid.autotests.cloud/wd/hub");
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "100.00");
+
+        Configuration.browser = config.getBrowserName();
+        Configuration.browserVersion = config.getBrowserVersion();
         Configuration.pageLoadStrategy = "eager";
+
+        if (config.isRemote()) {
+            Configuration.remote
+                    = config.getRemoteWebDriver();
+        }
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
